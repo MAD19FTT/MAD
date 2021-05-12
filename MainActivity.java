c2da58f1850c4f16ea9d37dc8f84da07f6408cbf
19FTT1066 <63836646+MAD19FTT@users.noreply.github.com>
2021-05-12 20:17:48 +0800

    button press and count up

--- MainActivity.java
+++ MainActivity.java
@@ -3,12 +3,28 @@ package com.example.android.tasbih;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int num = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void countUp(View view){
+        num++;
+        TextView counts = (TextView) findViewById(R.id.count);
+        counts.setText(num);
+    }
+
+    public void pressCount(View view){
+        num++;
+        TextView counts = (TextView) findViewById(R.id.count);
+        counts.setText(num);
+    }
+
 }
